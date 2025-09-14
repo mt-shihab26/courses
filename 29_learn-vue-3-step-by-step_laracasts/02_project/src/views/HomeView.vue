5ebe3ca9616ea938628715fca7045737bd59105f
Shihab Mahamud <shihab4t@gmail.com>
2025-09-15 04:50:02 +0600

    home view

--- 29_learn-vue-3-step-by-step_laracasts/02_project/src/views/HomeView.vue
+++ 29_learn-vue-3-step-by-step_laracasts/02_project/src/views/HomeView.vue
@@ -1,9 +1,50 @@
+<!-- <script lang="ts"> -->
+<!-- import { onMounted, ref } from 'vue' -->
+<!---->
+<!-- import TheWelcome from '../components/TheWelcome.vue' -->
+<!---->
+<!-- export default { -->
+<!--     components: { -->
+<!--         TheWelcome, -->
+<!--     }, -->
+<!--     setup() { -->
+<!--         const message = ref('Hello World') -->
+<!---->
+<!--         onMounted(() => { -->
+<!--             console.log('hi here') -->
+<!--         }) -->
+<!---->
+<!--         setTimeout(() => { -->
+<!--             message.value = 'Changed' -->
+<!--         }, 2000) -->
+<!---->
+<!--         return { -->
+<!--             message, -->
+<!--         } -->
+<!--     }, -->
+<!-- } -->
+<!-- </script> -->
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+
+import TheWelcome from '@/components/TheWelcome.vue'
+
+const message = ref('Hello World')
+
+onMounted(() => {
+    console.log('hi here')
+})
+
+setTimeout(() => {
+    message.value = 'Changed'
+}, 2000)
 </script>
 
 <template>
     <main>
+        <p>{{ message }}</p>
+        <input type="text" v-model="message" />
         <TheWelcome />
     </main>
 </template>
