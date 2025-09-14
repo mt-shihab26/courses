7ada83dafa7e4b2279bc3f4fa2d3216b39fc50ce
Shihab Mahamud <shihab4t@gmail.com>
2025-09-14 17:53:02 +0600

    wip

--- 29_learn-vue-3-step-by-step_laracasts/01_basic/dev.sh
+++ 29_learn-vue-3-step-by-step_laracasts/01_basic/dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bun x concurrently "bun x json-server --watch db.json" "bun x serve"
+bun x concurrently "bun x json-server --watch db.json --port 8000" "bun x serve" --names="server,client" -c "#93c5fd,#c4b5fd"
