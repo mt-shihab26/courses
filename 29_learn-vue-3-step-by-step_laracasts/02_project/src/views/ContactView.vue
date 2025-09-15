be43ce97d7ee36926fb5b95b21d13f0978b1c34e
Shihab Mahamud <shihab4t@gmail.com>
2025-09-15 22:56:28 +0600

    wip

--- 29_learn-vue-3-step-by-step_laracasts/02_project/src/views/ContactView.vue
+++ 29_learn-vue-3-step-by-step_laracasts/02_project/src/views/ContactView.vue
@@ -1,7 +1,30 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const textarea = ref<HTMLTextAreaElement | null>(null)
+
+onMounted(() => {
+    const element = textarea.value
+
+    element?.addEventListener('keydown', (e) => {
+        if (e.key === 'Tab') {
+            e.preventDefault()
+
+            const start = element.selectionStart
+            const end = element.selectionEnd
+
+            element.value = element.value.substring(0, start) + '\t' + element.value.substring(end)
+
+            element.selectionStart = element.selectionEnd = start + 1
+        }
+    })
+})
+</script>
+
 <template>
     <div>
         <form>
-            <textarea style="width: 100%; height: 300px">Hi There</textarea>
+            <textarea ref="textarea" style="width: 100%; height: 300px">Hi There</textarea>
         </form>
     </div>
 </template>
