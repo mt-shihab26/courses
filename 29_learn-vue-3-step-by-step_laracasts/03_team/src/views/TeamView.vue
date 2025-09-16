d3f9bd6dab41b999190f616df6e7eeaed690b59a
Shihab Mahamud <shihab4t@gmail.com>
2025-09-16 18:43:08 +0600

    team view

--- 29_learn-vue-3-step-by-step_laracasts/03_team/src/views/TeamView.vue
+++ 29_learn-vue-3-step-by-step_laracasts/03_team/src/views/TeamView.vue
@@ -9,19 +9,19 @@ import data from '@/data.json';
         <div>
             <button
                 class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-                :disabled="true"
+                :disabled="data.spots === data.members.length"
             >
-                Add Member (0 Spots Left)
+                Add Member ({{ data.spots - data.members.length }} Spots Left)
             </button>
         </div>
         <div>
             <div class="inline-flex items-center text-3xl relative">
                 <span class="text-yellow-500 text-3xl mr-2">😊</span>
-                <h3>Smiley Team</h3>
+                <h3>{{ data.name }}</h3>
                 <div
                     class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
                 >
-                    5
+                    {{ data.spots }}
                 </div>
             </div>
         </div>
