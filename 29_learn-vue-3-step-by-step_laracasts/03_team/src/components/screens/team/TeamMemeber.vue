a76338c9efe04ab110ace47f4907cfbfc0f0e4d5
Shihab Mahamud <shihab4t@gmail.com>
2025-09-16 17:51:55 +0600

    team member

---
+++ 29_learn-vue-3-step-by-step_laracasts/03_team/src/components/screens/team/TeamMemeber.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+type TMemeber = {
+    name: string;
+    email: string;
+    status: string;
+};
+
+defineProps<{
+    member: TMemeber;
+}>();
+</script>
+
+<template>
+    <tr class="bg-gray-100 px-12">
+        <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
+            <img src="https://i.pravatar.cc/50?u=sarah@doe.com" alt="" class="rounded-full" />
+            {{ member.name }}
+        </td>
+        <td class="text-gray-500 px-6 py-4">
+            {{ member.name }}
+        </td>
+        <td class="px-6 py-4">
+            <button class="text-green-400 text-xl">
+                {{ member.status }}
+            </button>
+        </td>
+    </tr>
+</template>
