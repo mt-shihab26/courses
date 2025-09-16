e622730bd7310e49b34122fd868a49f67b891753
Shihab Mahamud <shihab4t@gmail.com>
2025-09-16 17:37:55 +0600

    wip

--- 29_learn-vue-3-step-by-step_laracasts/03_team/src/main.ts
+++ 29_learn-vue-3-step-by-step_laracasts/03_team/src/main.ts
@@ -1,12 +1,16 @@
+import './main.css';
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './App.vue';
 import router from './router';
 
-const app = createApp(App);
+import App from './App.vue';
 
-app.use(createPinia());
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
