a1567b4ca4c8dc202bbf95e0d6ba351c61a3f4cf
Shihab Mahamud <shihab4t@gmail.com>
2025-09-16 17:30:28 +0600

    wip

--- 29_learn-vue-3-step-by-step_laracasts/03_team/src/views/TeamView.vue
+++ 29_learn-vue-3-step-by-step_laracasts/03_team/src/views/TeamView.vue
@@ -12,7 +12,7 @@
         </div>
         <div>
             <div class="inline-flex items-center text-3xl relative">
-                <img src="/smiley.png" alt="" class="mr-2" />
+                <span class="text-yellow-500 text-3xl mr-2">😊</span>
                 <h3>Smiley Team</h3>
                 <div
                     class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
