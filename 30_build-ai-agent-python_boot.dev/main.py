78c7ecab6458ef42bbd2960bbd56b53d5684736d
Shihab Mahamud <shihab4t@gmail.com>
2025-09-21 17:44:57 +0600

    wip

--- 30_build-ai-agent-python_boot.dev/main.py
+++ 30_build-ai-agent-python_boot.dev/main.py
@@ -1,5 +1,18 @@
+from os import environ
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from coding-agent!")
+    load_dotenv()
+    api_key = environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents="What is paystubhero?"
+    )
+
+    print(response.text)
 
 
 if __name__ == "__main__":
