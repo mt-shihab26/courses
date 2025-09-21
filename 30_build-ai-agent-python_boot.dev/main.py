3833cdc1ad0ce36361012dbcfc24798c744dcb31
Shihab Mahamud <shihab4t@gmail.com>
2025-09-21 18:13:49 +0600

    verbose

--- 30_build-ai-agent-python_boot.dev/main.py
+++ 30_build-ai-agent-python_boot.dev/main.py
@@ -2,6 +2,7 @@ from os import environ
 from dotenv import load_dotenv
 from google import genai
 from sys import argv, exit
+from google.genai import types
 
 
 def main():
@@ -9,19 +10,33 @@ def main():
     api_key = environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    if len(argv) < 2:
+    args = len(argv)
+    if args < 2:
         return exit(1)
+
+    verbose = False
+    if args == 3 and argv[2] == "--verbose":
+        verbose = True
 
     prompt = argv[1]
     model = "gemini-2.0-flash-001"
 
-    response = client.models.generate_content(model=model, contents=prompt)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    if verbose:
+        print("User prompt:", prompt)
+
+    response = client.models.generate_content(model=model, contents=messages)
     if response is None or response.usage_metadata is None:
         return
 
     print(response.text)
-    print(response.usage_metadata.prompt_token_count)
-    print(response.usage_metadata.candidates_token_count)
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
