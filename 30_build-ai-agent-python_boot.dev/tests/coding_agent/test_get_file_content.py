66a73e8c59674099277f1c7a709fec8f2498ef08
Shihab Mahamud <shihab4t@gmail.com>
2025-09-21 23:13:30 +0600

    wip

--- 30_build-ai-agent-python_boot.dev/tests/coding_agent/test_get_file_content.py
+++ 30_build-ai-agent-python_boot.dev/tests/coding_agent/test_get_file_content.py
@@ -2,12 +2,12 @@ from os import path
 from coding_agent.get_file_content import get_file_content
 
 
-def test_get_file_info_not_found():
+def test_returns_error_message_when_file_does_not_exist():
     response = get_file_content("data", "hello.txt")
     assert response == "Error: The file 'hello.txt' was not found."
 
 
-def test_get_file_info_provided():
+def test_returns_file_content_when_file_exists():
     response = get_file_content("data", "lorem.txt")
     with open(path.join("data", "lorem.txt"), "r") as file:
         file_content = file.read()
