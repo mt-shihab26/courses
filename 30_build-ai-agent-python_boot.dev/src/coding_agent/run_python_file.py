9a04ab32e31d3c2f606bab31e246967194e71d26
Shihab Mahamud <shihab4t@gmail.com>
2025-09-22 20:47:38 +0600

    run python file

--- 30_build-ai-agent-python_boot.dev/src/coding_agent/run_python_file.py
+++ 30_build-ai-agent-python_boot.dev/src/coding_agent/run_python_file.py
@@ -1,20 +1,28 @@
 from os import path
 
 
-def run_python_file(working_directory: str, file_name: str):
+def get_python_code(working_directory: str, file_name: str) -> str:
     target_path = path.abspath(path.join(working_directory, file_name))
 
     if not path.exists(target_path):
-        return f"Error: The file '{file_name}' was not found."
+        raise FileNotFoundError(f"The file '{file_name}' was not found.")
 
     if not path.isfile(target_path):
-        return f"Error: '{file_name}' is not a file."
+        raise ValueError(f"'{file_name}' is not a file.")
 
     if not target_path.endswith(".py"):
-        return f"Error: The file '{file_name}' was not python file."
+        raise ValueError(f"The file '{file_name}' was not python file.")
 
     try:
         with open(target_path, "r") as file:
             file_content = file.read()
+        return file_content
     except Exception as e:
-        return f"Error: reading file: {e}"
+        raise Exception(f"Error reading file: {e}")
+
+
+def run_python_file(working_directory: str, file_name: str):
+    try:
+        code = get_python_code(working_directory, file_name)
+    except Exception as e:
+        return f"Error: {e}"
