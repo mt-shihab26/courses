8b6b374ecda1cad5dfbc5300d6326f0c58e3afb9
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 01:31:14 +0600

    login

--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/login.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/login.tsx
@@ -1,6 +1,5 @@
 import { SocialConnections } from '@/components/screens/auth/social-connections';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
@@ -26,72 +25,66 @@ const Login = () => {
             keyboardDismissMode="interactive"
         >
             <View className="w-full gap-6">
-                <Card className="border-border/0 sm:border-border shadow-none sm:shadow-sm sm:shadow-black/5">
-                    <CardHeader>
-                        <CardTitle className="text-center text-xl sm:text-left">Sign in to your app</CardTitle>
-                        <CardDescription className="text-center sm:text-left">
-                            Welcome back! Please sign in to continue
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="gap-6">
-                        <View className="gap-6">
-                            <View className="gap-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    placeholder="m@example.com"
-                                    keyboardType="email-address"
-                                    autoComplete="email"
-                                    autoCapitalize="none"
-                                    onSubmitEditing={onEmailSubmitEditing}
-                                    returnKeyType="next"
-                                    submitBehavior="submit"
-                                />
-                            </View>
-                            <View className="gap-1.5">
-                                <View className="flex-row items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    <Button
-                                        variant="link"
-                                        size="sm"
-                                        className="web:h-fit ml-auto h-4 px-1 py-0 sm:h-4"
-                                        onPress={() => {
-                                            // TODO: Navigate to forgot password screen
-                                        }}
-                                    >
-                                        <Text className="leading-4 font-normal">Forgot your password?</Text>
-                                    </Button>
-                                </View>
-                                <Input
-                                    ref={passwordInputRef}
-                                    id="password"
-                                    secureTextEntry
-                                    returnKeyType="send"
-                                    onSubmitEditing={onSubmit}
-                                />
-                            </View>
-                            <Button className="w-full" onPress={onSubmit}>
-                                <Text>Continue</Text>
-                            </Button>
-                        </View>
-                        <Text className="text-center text-sm">
-                            Don&apos;t have an account?{' '}
-                            <Pressable
+                <Text className="text-center text-xl sm:text-left">Sign in to your app</Text>
+                <Text className="text-center sm:text-left text-muted-foreground">
+                    Welcome back! Please sign in to continue
+                </Text>
+                <View className="gap-6">
+                    <View className="gap-1.5">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            id="email"
+                            placeholder="m@example.com"
+                            keyboardType="email-address"
+                            autoComplete="email"
+                            autoCapitalize="none"
+                            onSubmitEditing={onEmailSubmitEditing}
+                            returnKeyType="next"
+                            submitBehavior="submit"
+                        />
+                    </View>
+                    <View className="gap-1.5">
+                        <View className="flex-row items-center">
+                            <Label htmlFor="password">Password</Label>
+                            <Button
+                                variant="link"
+                                size="sm"
+                                className="web:h-fit ml-auto h-4 px-1 py-0 sm:h-4"
                                 onPress={() => {
-                                    // TODO: Navigate to sign up screen
+                                    // TODO: Navigate to forgot password screen
                                 }}
                             >
-                                <Text className="text-sm underline underline-offset-4">Sign up</Text>
-                            </Pressable>
-                        </Text>
-                        <View className="flex-row items-center">
-                            <Separator className="flex-1" />
-                            <Text className="text-muted-foreground px-4 text-sm">or</Text>
-                            <Separator className="flex-1" />
+                                <Text className="leading-4 font-normal">Forgot your password?</Text>
+                            </Button>
                         </View>
-                        <SocialConnections />
-                    </CardContent>
-                </Card>
+                        <Input
+                            ref={passwordInputRef}
+                            id="password"
+                            secureTextEntry
+                            returnKeyType="send"
+                            onSubmitEditing={onSubmit}
+                        />
+                    </View>
+                    <Button className="w-full" onPress={onSubmit}>
+                        <Text>Continue</Text>
+                    </Button>
+                </View>
+                <Text className="text-center text-sm">
+                    Don&apos;t have an account?{' '}
+                    <Pressable
+                        onPress={() => {
+                            // TODO: Navigate to sign up screen
+                        }}
+                    >
+                        <Text className="text-sm underline underline-offset-4">Sign up</Text>
+                    </Pressable>
+                </Text>
+                <View className="flex-row items-center">
+                    <Separator className="flex-1" />
+                    <Text className="text-muted-foreground px-4 text-sm">or</Text>
+                    <Separator className="flex-1" />
+                </View>
+                <SocialConnections />
             </View>
         </ScrollView>
     );
