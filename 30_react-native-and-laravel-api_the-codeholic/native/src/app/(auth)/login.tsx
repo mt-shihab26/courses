e3469509d3debbf473d69dbda290c3e5e683013b
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 01:17:27 +0600

    auth routes

---
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/login.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Login = () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-foreground">Login</Text>
+        </View>
+    );
+};
+
+export default Login;
--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/(tabs)/index.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 const Index = () => {
     return (
         <View className="flex-1 items-center justify-center">
+            <Link className="text-foreground" href="/login">
+                Login
+            </Link>
             <Text className="text-foreground">Tab Home 2</Text>
         </View>
     );
