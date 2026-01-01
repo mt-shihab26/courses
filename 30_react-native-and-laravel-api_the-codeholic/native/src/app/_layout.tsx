bd5cf3b64d892a57704d0dfe854ba83480415c11
Shihab Mahamud <shihab4t@gmail.com>
2026-01-01 20:17:13 +0600

    shadcn/ui

--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/_layout.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/_layout.tsx
@@ -1,9 +1,15 @@
 import './global.css';
 
+import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 
 const Layout = () => {
-    return <Stack />;
+    return (
+        <>
+            <Stack />
+            <PortalHost />
+        </>
+    );
 };
 
 export default Layout;
