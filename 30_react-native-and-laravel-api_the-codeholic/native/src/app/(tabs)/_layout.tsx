89f6a14058b1e8b79e70b2d04842504ee8cd350b
Shihab Mahamud <shihab4t@gmail.com>
2026-01-01 21:49:28 +0600

    layout

--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/(tabs)/_layout.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(tabs)/_layout.tsx
@@ -1,11 +1,14 @@
 import { useTheme } from '@/lib/theme';
+import { useRouter } from 'expo-router';
 
-import { BackPressable } from '@/components/elements/back-pressable';
+import { Button } from '@/components/ui/button';
 import { Tabs } from 'expo-router';
-import { HouseIcon, SettingsIcon } from 'lucide-react-native';
+import { ChevronLeftIcon, HouseIcon, SettingsIcon } from 'lucide-react-native';
+import { Pressable } from 'react-native';
 
 const Layout = () => {
     const { theme } = useTheme();
+    const { back } = useRouter();
 
     return (
         <Tabs screenOptions={{ tabBarActiveTintColor: theme.primary }}>
@@ -21,7 +24,13 @@ const Layout = () => {
                 options={{
                     title: 'Settings',
                     tabBarIcon: ({ size, color }) => <SettingsIcon size={size} color={color} />,
-                    headerLeft: BackPressable,
+                    headerLeft: () => (
+                        <Pressable onPress={() => back()} className="mr-2 ml-4 flex-1 items-center">
+                            <Button variant="ghost" size="icon">
+                                <ChevronLeftIcon className="pill-primary size-4" />
+                            </Button>
+                        </Pressable>
+                    ),
                 }}
             />
         </Tabs>
