5ee430754f1f26be97f03d08bd020c5e42d01de4
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 01:42:45 +0600

    register

---
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/forgot-password.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const ForgotPassword = () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-foreground">Register</Text>
+        </View>
+    );
+};
+
+export default ForgotPassword;
--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/login.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/login.tsx
@@ -1,22 +1,20 @@
+import { useRef } from 'react';
+
 import { SocialConnections } from '@/components/screens/auth/social-connections';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
 import { Text } from '@/components/ui/text';
-import { useRef } from 'react';
-import { Pressable, ScrollView, TextInput, View } from 'react-native';
+import { Link } from 'expo-router';
+import { ScrollView, TextInput, View } from 'react-native';
 
 const Login = () => {
     const passwordInputRef = useRef<TextInput>(null);
 
-    function onEmailSubmitEditing() {
-        passwordInputRef.current?.focus();
-    }
-
-    function onSubmit() {
-        // TODO: Submit form and navigate to protected screen if successful
-    }
+    const handleSubmit = () => {
+        //
+    };
 
     return (
         <ScrollView
@@ -26,7 +24,7 @@ const Login = () => {
         >
             <View className="w-full gap-6">
                 <Text className="text-center text-xl sm:text-left">Sign in to your app</Text>
-                <Text className="text-center sm:text-left text-muted-foreground">
+                <Text className="text-muted-foreground text-center sm:text-left">
                     Welcome back! Please sign in to continue
                 </Text>
                 <View className="gap-6">
@@ -38,7 +36,7 @@ const Login = () => {
                             keyboardType="email-address"
                             autoComplete="email"
                             autoCapitalize="none"
-                            onSubmitEditing={onEmailSubmitEditing}
+                            onSubmitEditing={() => passwordInputRef.current?.focus()}
                             returnKeyType="next"
                             submitBehavior="submit"
                         />
@@ -46,38 +44,29 @@ const Login = () => {
                     <View className="gap-1.5">
                         <View className="flex-row items-center">
                             <Label htmlFor="password">Password</Label>
-                            <Button
-                                variant="link"
-                                size="sm"
-                                className="web:h-fit ml-auto h-4 px-1 py-0 sm:h-4"
-                                onPress={() => {
-                                    // TODO: Navigate to forgot password screen
-                                }}
-                            >
-                                <Text className="leading-4 font-normal">Forgot your password?</Text>
-                            </Button>
+                            <Link href="/forgot-password">
+                                <Button variant="link" size="sm" className="web:h-fit ml-auto h-4 px-1 py-0 sm:h-4">
+                                    <Text className="leading-4 font-normal">Forgot your password?</Text>
+                                </Button>
+                            </Link>
                         </View>
                         <Input
                             ref={passwordInputRef}
                             id="password"
                             secureTextEntry
                             returnKeyType="send"
-                            onSubmitEditing={onSubmit}
+                            onSubmitEditing={handleSubmit}
                         />
                     </View>
-                    <Button className="w-full" onPress={onSubmit}>
+                    <Button className="w-full" onPress={handleSubmit}>
                         <Text>Continue</Text>
                     </Button>
                 </View>
-                <Text className="text-center text-sm">
+                <Text className="flex-1 items-center text-center text-sm">
                     Don&apos;t have an account?{' '}
-                    <Pressable
-                        onPress={() => {
-                            // TODO: Navigate to sign up screen
-                        }}
-                    >
-                        <Text className="text-sm underline underline-offset-4">Sign up</Text>
-                    </Pressable>
+                    <Link href="/register" className="text-sm underline underline-offset-4">
+                        Sign up
+                    </Link>
                 </Text>
                 <View className="flex-row items-center">
                     <Separator className="flex-1" />
