fcef438179d992cde2ab7936a6cd3f5f5ef01b6f
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 03:51:13 +0600

    layout

---
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="login" options={{ title: 'Sign In' }} />
+            <Stack.Screen name="register" options={{ title: 'Sign Up' }} />
+            <Stack.Screen name="forgot-password" options={{ title: 'Forgot Password' }} />
+            <Stack.Screen name="reset-password" options={{ title: 'Reset Password' }} />
+            <Stack.Screen name="verify-email" options={{ title: 'Verify Email' }} />
+        </Stack>
+    );
+};
+
+export default Layout;
--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/_layout.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/_layout.tsx
@@ -18,6 +18,7 @@ const Layout = () => {
         <ThemeProvider value={NAV_THEME[theme ?? 'light']}>
             <Stack>
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
             </Stack>
             <PortalHost />
         </ThemeProvider>
