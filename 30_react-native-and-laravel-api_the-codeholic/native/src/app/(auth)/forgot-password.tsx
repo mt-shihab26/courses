2d3a590ca336f43d63864a9736889c53642780fc
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 03:20:27 +0600

    forgot password

--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/forgot-password.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(auth)/forgot-password.tsx
@@ -1,8 +1,7 @@
+import { EmailInput } from '@/components/inputs/email-input';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Text } from '@/components/ui/text';
-import { ScrollView, View } from 'react-native';
+import { AuthWrapper } from '@/components/wrappers/auth-wrapper';
 
 const ForgotPassword = () => {
     const handleSubmit = () => {
@@ -10,35 +9,19 @@ const ForgotPassword = () => {
     };
 
     return (
-        <ScrollView
-            keyboardShouldPersistTaps="handled"
-            contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe"
-            keyboardDismissMode="interactive"
-        >
-            <View className="w-full gap-6">
-                <Text className="text-center text-xl sm:text-left">Forgot password?</Text>
-                <Text className="text-muted-foreground text-center sm:text-left">
-                    Enter your email to reset your password
-                </Text>
-                <View className="gap-6">
-                    <View className="gap-1.5">
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            id="email"
-                            placeholder="m@example.com"
-                            keyboardType="email-address"
-                            autoComplete="email"
-                            autoCapitalize="none"
-                            returnKeyType="send"
-                            onSubmitEditing={handleSubmit}
-                        />
-                    </View>
-                    <Button className="w-full" onPress={handleSubmit}>
-                        <Text>Reset your password</Text>
-                    </Button>
-                </View>
-            </View>
-        </ScrollView>
+        <AuthWrapper title="Forgot password?" description="Enter your email to reset your password">
+            <EmailInput
+                id="email"
+                required={true}
+                value=""
+                error=""
+                onChange={() => {}}
+                onSubmitEditing={handleSubmit}
+            />
+            <Button className="w-full" onPress={handleSubmit}>
+                <Text>Reset your password</Text>
+            </Button>
+        </AuthWrapper>
     );
 };
 
