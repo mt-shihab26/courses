51c5a65873429c0d930f4fe38beb127e960de730
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 02:14:43 +0600

    verify email

--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/(tabs)/index.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 const Index = () => {
     return (
-        <View className="flex-1 items-center justify-center">
-            <Link className="text-foreground" href="/login">
-                Login
+        <View className="flex-1 items-center justify-center gap-2">
+            <Link className="text-foreground" href="/login" asChild>
+                <Button>
+                    <Text>Login</Text>
+                </Button>
             </Link>
-            <Text className="text-foreground">Tab Home 2</Text>
+            <Link className="text-foreground" href="/verify-email" asChild>
+                <Button>
+                    <Text>Verify Email</Text>
+                </Button>
+            </Link>
         </View>
     );
 };
