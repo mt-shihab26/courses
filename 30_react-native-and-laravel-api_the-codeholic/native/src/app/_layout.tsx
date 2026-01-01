47336e5cf7efc819da406e595e3fed2f32cafcdf
Shihab Mahamud <shihab4t@gmail.com>
2026-01-01 20:28:13 +0600

    uniwind

--- 30_react-native-and-laravel-api_the-codeholic/native/src/app/_layout.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/app/_layout.tsx
@@ -1,15 +1,25 @@
-import './global.css';
+import '@/global.css';
 
+import { NAV_THEME } from '@/lib/theme';
+import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useUniwind } from 'uniwind';
 
-const Layout = () => {
-    return (
-        <>
-            <Stack />
-            <PortalHost />
-        </>
-    );
-};
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
-export default Layout;
+export default function RootLayout() {
+  const { theme } = useUniwind();
+
+  return (
+    <ThemeProvider value={NAV_THEME[theme ?? 'light']}>
+      <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
+      <Stack />
+      <PortalHost />
+    </ThemeProvider>
+  );
+}
