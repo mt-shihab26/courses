72fc9d2c1a9b6035058a835789d694b815cdc4e0
Shihab Mahamud <shihab4t@gmail.com>
2026-01-02 16:01:01 +0600

    theme

--- 30_react-native-and-laravel-api_the-codeholic/native/src/providers/theme.tsx
+++ 30_react-native-and-laravel-api_the-codeholic/native/src/providers/theme.tsx
@@ -1,4 +1,4 @@
-import { store, useStore } from '@/hooks/use-store';
+import { useStore } from '@/hooks/use-store';
 import { NAV_THEME } from '@/lib/theme';
 import { TTheme } from '@/types/utils';
 import { ThemeProvider as NavigationThemeProvider } from '@react-navigation/native';
@@ -8,29 +8,24 @@ import { Uniwind, useUniwind } from 'uniwind';
 const THEME_KEY = 'app-theme-preference';
 
 export const useTheme = () => {
-    const [loading, storedTheme, setStoredTheme] = useStore(THEME_KEY);
-
-    useEffect(() => {
-        const init = async () => {
-            const savedTheme = (await store.get(THEME_KEY)) as TTheme | null;
-            if (savedTheme) {
-                Uniwind.setTheme(savedTheme);
-            }
-        };
-
-        init();
-    }, []);
+    const [loading, storedTheme, setStoredTheme] = useStore<TTheme>(THEME_KEY);
 
     const { theme: scheme, hasAdaptiveThemes } = useUniwind();
 
     const theme: TTheme = hasAdaptiveThemes ? 'system' : scheme;
 
+    useEffect(() => {
+        if (storedTheme) {
+            Uniwind.setTheme(storedTheme);
+        }
+    }, [storedTheme]);
+
     const setTheme = async (theme: TTheme) => {
-        Uniwind.setTheme(theme);
-        await store.set(THEME_KEY, theme);
+        await setStoredTheme(theme);
     };
 
     return {
+        loading,
         scheme,
         theme,
         setTheme,
