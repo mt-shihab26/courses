a1b1472ebdda42538bc9df65ee6834bbd1ef49a4
Shihab Mahamud <shihab4t@gmail.com>
2026-01-13 12:36:39 +0600

    about page

--- 30_build-a-password-manager-rails-8_nick-mcneany/config/routes.rb
+++ 30_build-a-password-manager-rails-8_nick-mcneany/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   get "/dashboard" => "dashboard#index"
+  get "/about" => "dashboard#about"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "dashboard#index"
 end
