e29770a52fa5f0cafa71aab083420658627c0450
Shihab Mahamud <shihab4t@gmail.com>
2026-02-06 00:29:15 +0600

    wip

---
+++ 30_php-the-right-way_program-with-gio/04_data_types.php
@@ -0,0 +1,17 @@
+<?php
+
+$completed = true;
+
+echo gettype($completed)."\n";
+
+$score = 75;
+
+echo gettype($score)."\n";
+
+$price = 0.99;
+
+echo gettype($price)."\n";
+
+$greeting = 'Hello';
+
+echo gettype($greeting)."\n";
