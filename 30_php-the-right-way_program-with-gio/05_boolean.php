770f507d26c3d33584188dcbf1a971672613c192
Shihab Mahamud <shihab4t@gmail.com>
2026-02-07 03:26:42 +0600

    wip

---
+++ 30_php-the-right-way_program-with-gio/05_boolean.php
@@ -0,0 +1,16 @@
+<?php
+
+$completed = false;
+/* $completed = FALSE; */ // all the same
+/* $completed = False; */ // all the same
+/* $completed = FaLse; */ // all the same
+
+$completed = true;
+
+echo $completed."\n";
+
+var_dump(0 == false, -0 == false); // false
+var_dump(0.0 == false, -0.0 == false); // false
+var_dump('' == false); // false
+var_dump([] == false); // false
+var_dump(null == false); // false
