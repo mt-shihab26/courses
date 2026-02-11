1f404dec15cafc269e46a55e1cc3a9f6a1673dde
Shihab Mahamud <shihab4t@gmail.com>
2026-02-12 04:54:16 +0600

    integer

---
+++ 30_php-the-right-way_program-with-gio/06_integer.php
@@ -0,0 +1,10 @@
+<?php
+
+echo PHP_INT_MIN."\n";
+echo PHP_INT_MAX."\n";
+echo PHP_INT_SIZE."\n";
+
+echo (string) 0b11 ."\n"; // binary
+echo (string) 11 ."\n"; // decimal
+echo (string) 011 ."\n"; // octal
+echo (string) 0x11 ."\n"; // hex-decimal
