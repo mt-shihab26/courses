fb6267e66092e133d754a340c5ef1260d70182fb
Shihab Mahamud <shihab4t@gmail.com>
2026-02-12 04:57:24 +0600

    integer

--- 30_php-the-right-way_program-with-gio/06_integer.php
+++ 30_php-the-right-way_program-with-gio/06_integer.php
@@ -8,3 +8,11 @@ echo (string) 0b11 ."\n"; // binary
 echo (string) 11 ."\n"; // decimal
 echo (string) 011 ."\n"; // octal
 echo (string) 0x11 ."\n"; // hex-decimal
+
+var_dump(PHP_INT_MAX + 1);
+
+/* var_dump((integer) PHP_INT_MAX + 1); */
+var_dump((int) PHP_INT_MAX + 1);
+
+echo (string) (int) true."\n";
+echo (string) (int) false."\n";
