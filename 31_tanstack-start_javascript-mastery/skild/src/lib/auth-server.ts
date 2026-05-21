4b1879b13c523c13ad259ffa141610c2b016ba57
Shihab Mahamud <shihab4t@gmail.com>
2026-05-21 09:40:32 +0600

    auth server

--- 31_tanstack-start_javascript-mastery/skild/src/lib/auth-server.ts
+++ 31_tanstack-start_javascript-mastery/skild/src/lib/auth-server.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
 
-export const auth = betterAuth({
+export const authServer = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
--- 31_tanstack-start_javascript-mastery/skild/src/routes/api/auth/$.ts
+++ 31_tanstack-start_javascript-mastery/skild/src/routes/api/auth/$.ts
@@ -1,11 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { auth } from '#/lib/auth'
+import { authServer } from '#/lib/auth-server'
 
 export const Route = createFileRoute('/api/auth/$')({
     server: {
         handlers: {
-            GET: ({ request }) => auth.handler(request),
-            POST: ({ request }) => auth.handler(request),
+            GET: ({ request }) => authServer.handler(request),
+            POST: ({ request }) => authServer.handler(request),
         },
     },
 })
