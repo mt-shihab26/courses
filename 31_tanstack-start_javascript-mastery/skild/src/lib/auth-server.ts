61629c0d6ebcc08207c706623c9de2cb525d8485
Shihab Mahamud <shihab4t@gmail.com>
2026-05-21 09:54:18 +0600

    wip

--- 31_tanstack-start_javascript-mastery/skild/src/lib/auth-server.ts
+++
@@ -1,9 +0,0 @@
-import { betterAuth } from 'better-auth'
-import { tanstackStartCookies } from 'better-auth/tanstack-start'
-
-export const authServer = betterAuth({
-    emailAndPassword: {
-        enabled: true,
-    },
-    plugins: [tanstackStartCookies()],
-})
